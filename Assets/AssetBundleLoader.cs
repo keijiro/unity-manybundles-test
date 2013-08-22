fd159eb6d73f1a47bfd08fa3f4a6373f3913dc5e
Takahashi Keijiro <keijiro@Super-MSX.local>
2013-08-22 10:39:46 +0900

    Initial commit.

---
+++ Assets/AssetBundleLoader.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AssetBundleLoader : MonoBehaviour
+{
+    public string baseUrl = "https://github.com/keijiro/unity-manybundles-test/raw/master/Bundles/";
+    public int numberOfFiles = 1024;
+    string lastLoaded = "";
+
+    IEnumerator Start ()
+    {
+        for (var i = 0; i < numberOfFiles; i++) {
+            var url = baseUrl + "bundle" + i.ToString ("0000") + ".unity3d";
+            var www = WWW.LoadFromCacheOrDownload (url, 0);
+            yield return www;
+            if (www.error != null) {
+                lastLoaded = www.error.ToString();
+                break;
+            }
+            lastLoaded = www.assetBundle.mainAsset.name;
+        }
+    }
+
+    void OnGUI ()
+    {
+        GUI.Label (new Rect (32, 32, 200, 200), lastLoaded); 
+    }
+}
---
+++ Assets/Editor/AssetBundleTool.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public static class AssetBundleTool {
+    [MenuItem("Asset Bundle/Build asset bundles")]
+    static void BuildAssetBundles() {
+        var options = BuildAssetBundleOptions.CompleteAssets;
+        for (var i = 0; i < 1024; i++) {
+            var path = "Assets/Dataset/file" + i.ToString("0000") + ".bytes";
+            var asset = AssetDatabase.LoadMainAssetAtPath(path) as TextAsset;
+            Object[] assets = { asset };
+            var destPath = "Bundles/bundle" + i.ToString("0000") + ".unity3d";
+            BuildPipeline.BuildAssetBundle(asset, assets, destPath, options, BuildTarget.iPhone);
+        }
+    }
+}
